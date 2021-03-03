332b88726d389aef819f9b3b1885ada6e5714723
davisengland <davisrodneyengland@gmail.com>
2021-03-03 16:17:25 -0700

    Finished app-3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      userInput: '',
+      arr: ['spaghetti', 'ice cream', 'sushi', 'pizza', 'cereal', 'tacos']
+    }
+  }
+
+  handleChange(val) {
+    this.setState({userInput: val})
+  }
+
+  render() {
+    let newArr = this.state.arr.filter(elem => elem.includes(this.state.userInput)).map(elem => <h2>{elem}</h2>)
+
+    return (
+      <div className="App">
+        <input onChange={event  => this.handleChange(event.target.value)}/>
+        {newArr}
+      </div>
+    )
+  }
 }
 
 export default App;
