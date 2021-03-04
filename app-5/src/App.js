ffe6d9cd37c5eb136060d659eaf042033e841d07
davisengland <davisrodneyengland@gmail.com>
2021-03-03 17:46:54 -0700

    Finished app-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Image from './Components/Image'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Image/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Image url={"https://cdn.pixabay.com/photo/2016/05/24/16/48/mountains-1412683__340.png"}/>
+      </div>
+    )
+  }
 }
 
 export default App;
